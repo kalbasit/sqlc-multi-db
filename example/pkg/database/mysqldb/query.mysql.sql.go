ba4e95b0e75b62b87bbfa58aabc42e95fd8ad20e
Wael Nasreddine <wael.nasreddine@gmail.com>
2026-03-22 21:04:51 -0700

    whatever

--- example/pkg/database/mysqldb/query.mysql.sql.go
+++ example/pkg/database/mysqldb/query.mysql.sql.go
@@ -152,6 +152,9 @@ func (q *Queries) GetBookTags(ctx context.Context, bookID int64) ([]Tag, error) 
 		}
 		items = append(items, i)
 	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
 	if err := rows.Err(); err != nil {
 		return nil, err
 	}
@@ -189,6 +192,9 @@ func (q *Queries) GetBooksByAuthor(ctx context.Context, author string) ([]Book, 
 			return nil, err
 		}
 		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
@@ -250,6 +256,9 @@ func (q *Queries) ListBooks(ctx context.Context) ([]Book, error) {
 			return nil, err
 		}
 		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
 	}
 	if err := rows.Err(); err != nil {
 		return nil, err
